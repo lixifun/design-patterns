9e1be4d8d7a9f6ccba3eb1b41ca27a39275c9993
lixifun <lixifun@qq.com>
2020-01-15 20:58:37 +0800

    第 10 章 模板方法模式 10.2 提炼代码

---
+++ src/main/java/fun/lixi/study/designpatterns/p10d3/TestPaperAMod.java
@@ -0,0 +1,19 @@
+package fun.lixi.study.designpatterns.p10d3;
+
+public class TestPaperAMod extends TestPaperMod {
+
+    @Override
+    public String answer1() {
+        return "b";
+    }
+
+    @Override
+    public String answer2() {
+        return "c";
+    }
+
+    @Override
+    public String answer3() {
+        return "a";
+    }
+}
---
+++ src/main/java/fun/lixi/study/designpatterns/p10d3/TestPaperB.java
@@ -0,0 +1,22 @@
+package fun.lixi.study.designpatterns.p10d3;
+
+public class TestPaperB extends TestPaper {
+
+    @Override
+    public void testQuestion1() {
+        super.testQuestion1();
+        System.out.println("答案: b");
+    }
+
+    @Override
+    public void testQuestion2() {
+        super.testQuestion2();
+        System.out.println("答案: b");
+    }
+
+    @Override
+    public void testQuestion3() {
+        super.testQuestion3();
+        System.out.println("答案: b");
+    }
+}
