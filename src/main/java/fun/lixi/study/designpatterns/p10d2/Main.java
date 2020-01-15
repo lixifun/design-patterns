0f5a87f3b325fbfa9ad05267b90e521a564b663e
lixifun <lixifun@qq.com>
2020-01-15 20:47:25 +0800

    第 10 章 模板方法模式 10.2 重复=易错+难改

---
+++ src/main/java/fun/lixi/study/designpatterns/p10d2/Main.java
@@ -0,0 +1,20 @@
+package fun.lixi.study.designpatterns.p10d2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("学生甲抄的试卷：");
+        TestPaperA studentA = new TestPaperA();
+
+        studentA.testQuestion1();
+        studentA.testQuestion2();
+        studentA.testQuestion3();
+
+        System.out.println("学生乙抄的试卷：");
+        TestPaperB studentB = new TestPaperB();
+
+        studentB.testQuestion1();
+        studentB.testQuestion2();
+        studentB.testQuestion3();
+
+    }
+}
