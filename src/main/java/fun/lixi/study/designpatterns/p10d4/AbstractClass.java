4db4ddf755737b2f5531fac52ccd26462bc5d178
lixifun <lixifun@qq.com>
2020-01-15 21:10:49 +0800

    第 10 章 模板方法模式 10.4 模板方法模式

---
+++ src/main/java/fun/lixi/study/designpatterns/p10d4/AbstractClass.java
@@ -0,0 +1,13 @@
+package fun.lixi.study.designpatterns.p10d4;
+
+public abstract class AbstractClass {
+
+    public abstract void primitiveOperation1();
+    public abstract void primitiveOperation2();
+
+    public void templateMethod() {
+        primitiveOperation1();
+        primitiveOperation2();
+        System.out.println("");
+    }
+}
