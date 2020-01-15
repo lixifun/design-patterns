bf5911246a4cf9b9cdd0cc0e81748b814851696d
lixifun <lixifun@qq.com>
2020-01-15 21:47:19 +0800

    第 14 章 观察者模式 14.2 双向耦合的代码

---
+++ src/main/java/fun/lixi/study/designpatterns/p14d2/Main.java
@@ -0,0 +1,18 @@
+package fun.lixi.study.designpatterns.p14d2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Secretary tongzhizhe = new Secretary();
+
+        StockObserver tongshi1 = new StockObserver("o1", tongzhizhe);
+        StockObserver tongshi2 = new StockObserver("o2", tongzhizhe);
+
+        tongzhizhe.attach(tongshi1);
+        tongzhizhe.attach(tongshi2);
+
+        tongzhizhe.setAction("老板回来了！");
+        tongzhizhe.notifyMethod();
+    }
+
+}
---
+++ src/main/java/fun/lixi/study/designpatterns/p14d2/Secretary.java
@@ -0,0 +1,33 @@
+package fun.lixi.study.designpatterns.p14d2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Secretary {
+
+    private List<StockObserver> observers = new ArrayList<>();
+    private String action;
+
+    /**
+     * 增加
+     */
+    public void attach(StockObserver observer) {
+        observers.add(observer);
+    }
+
+    /**
+     * 通知
+     */
+    public void notifyMethod() {
+        observers.forEach(StockObserver::update);
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
+
