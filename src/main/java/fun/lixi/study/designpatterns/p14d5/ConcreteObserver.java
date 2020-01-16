17b57ca41cc6a519d3423d17f393ff09cc72d4c1
lixifun <lixifun@qq.com>
2020-01-16 21:19:07 +0800

    第 14 章 观察者模式 14.5

---
+++ src/main/java/fun/lixi/study/designpatterns/p14d5/ConcreteObserver.java
@@ -0,0 +1,27 @@
+package fun.lixi.study.designpatterns.p14d5;
+
+public class ConcreteObserver extends Observer {
+
+    private String name;
+    private String observerState;
+    private ConcreteSubject subject;
+
+    public ConcreteObserver(ConcreteSubject subject, String name) {
+        this.name = name;
+        this.subject = subject;
+    }
+
+    @Override
+    public void update() {
+        observerState = subject.getSubjectState();
+        System.out.println(String.format("观察者 %s 的新状态是 %s", name, observerState));
+    }
+
+    public ConcreteSubject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(ConcreteSubject subject) {
+        this.subject = subject;
+    }
+}
---
+++ src/main/java/fun/lixi/study/designpatterns/p14d5/Observer.java
@@ -0,0 +1,10 @@
+package fun.lixi.study.designpatterns.p14d5;
+
+/**
+ * 这里后面用的 interface, 可以直接查看
+ * @see java.util.Observer
+ */
+public abstract class Observer {
+
+    public abstract void update();
+}
